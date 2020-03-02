eaea8fecfac0df27aec5741a46ab7094c512f21d
iRuxu <rx6@qq.com>
2020-03-02 17:24:13 +0800

    fix export

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 const JX3BOX = {
     // 根目录
-    __Root : 'https://www.jx3box.com/',
+    __Root: "https://www.jx3box.com/",
     // OSS
-    __ossRoot : 'https://oss.jx3box.com/',
+    __ossRoot: "https://oss.jx3box.com/",
     // OSS MIRROR
-    __ossMirror : 'https://console.cnyixun.com/',
+    __ossMirror: "https://console.cnyixun.com/",
     // 图标目录
-    __iconPath : this.__ossRoot + 'icon/',
+    __iconPath: this.__ossRoot + "icon/",
 
     // SSO服务
-    __ssoRoot : 'https://sso.jx3box.com/',
+    __ssoRoot: "https://sso.jx3box.com/",
     // APP服务
-    __nodeRoot : 'https://node.jx3box.com/',
+    __nodeRoot: "https://node.jx3box.com/",
     // 成就服务
-    __helperUrl : 'https://helper.jx3box.com/',
+    __helperUrl: "https://helper.jx3box.com/",
     // 边缘支撑服务（爬虫、官方接口）
-    __spider : 'https://spider.jx3box.com/',
+    __spider: "https://spider.jx3box.com/",
     // 搜索服务
-    __searchRoot : 'https://search.jx3box.com/',
-    
-    // 文章类型映射
-    __postType : {
-        'posts':'论坛',
-        'macro' : '剑三宏库',
-        'jx3dat' : '插件数据',
-        'fb' : '副本攻略',
-        'bps' : '职业攻略',
-        'cj' : '成就百科',
-        'share' : '捏脸分享',
-        'tool' : '教程工具',
-        'help' : '帮助文档',
-        'post' : '茶馆交流',
-        'page' : '系统页面'
-    },
-    
-}
+    __searchRoot: "https://search.jx3box.com/",
 
-export default JX3BOX 
+    // 文章类型映射
+    __postType: {
+        posts: "论坛",
+        macro: "剑三宏库",
+        jx3dat: "插件数据",
+        fb: "副本攻略",
+        bps: "职业攻略",
+        cj: "成就百科",
+        share: "捏脸分享",
+        tool: "教程工具",
+        help: "帮助文档",
+        post: "茶馆交流",
+        page: "系统页面"
+    }
+};
+
+module.exports = { JX3BOX };
