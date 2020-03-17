399875d653bf092901d3c1a062b9faa2534b746e
iRuxu <rx6@qq.com>
2020-03-17 17:07:19 +0800

    update utils about img cdn

--- utils.js
+++ utils.js
@@ -1,5 +1,16 @@
 module.exports = {
-    resolveImagePath : function (url){
-        return url.replace(/oss.jx3box.com/g,'console.cnyixun.com')
+    resolveImagePath : function (str){
+        return str.replace(/oss\.jx3box\.com/g,'console.cnyixun.com')
+    },
+
+    checkImageLoad : function (jq){
+        jq.one("error", function(e) {
+            var img_url = $(this).attr("src");
+            var fix_url = img_url.replace(
+                /console\.cnyixun\.com/g,
+                "oss.jx3box.com"
+            );
+            $(this).attr("src", fix_url);
+        });
     }
 }
