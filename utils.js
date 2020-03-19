71c99b4c8b4967aa15739b1676e94f4fe61e0a58
iRuxu <rx6@qq.com>
2020-03-19 19:25:19 +0800

    fix

--- utils.js
+++ utils.js
@@ -1,6 +1,8 @@
+const dict = require('./includes/dict.js');
+
 module.exports = {
     resolveImagePath : function (str){
-        return str && str.replace(/oss\.jx3box\.com/g,'console.cnyixun.com')
+        return (str && str.length) ?  str.replace(/oss\.jx3box\.com/g,'console.cnyixun.com') : ''
     },
 
     checkImageLoad : function (jq){
@@ -13,5 +15,19 @@ module.exports = {
             );
             $(this).attr("src", fix_url);
         })
+    },
+
+    cn2tw : function (str){
+        if(str.length){
+            let arr = new Array(str.length)
+            let i = -1
+            for(let char of str){
+                i = dict['cn'].indexOf(char)
+                i > -1 ? arr.push(dict['tw'][i]) : arr.push(char)
+            }
+            return arr.join('')
+        }else{
+            return ''
+        }
     }
 }
