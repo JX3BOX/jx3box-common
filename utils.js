bd9f0216557c7edb0b1017d237360ef41a875b50
iRuxu <rx6@qq.com>
2020-03-19 13:00:12 +0800

    update

--- utils.js
+++ utils.js
@@ -1,16 +1,17 @@
 module.exports = {
     resolveImagePath : function (str){
-        return str.replace(/oss\.jx3box\.com/g,'console.cnyixun.com')
+        return str && str.replace(/oss\.jx3box\.com/g,'console.cnyixun.com')
     },
 
     checkImageLoad : function (jq){
-        jq.one("error", function(e) {
+        jq.length &&
+        jq.one('error',function (){
             var img_url = $(this).attr("src");
             var fix_url = img_url.replace(
                 /console\.cnyixun\.com/g,
                 "oss.jx3box.com"
             );
             $(this).attr("src", fix_url);
-        });
+        })
     }
 }
