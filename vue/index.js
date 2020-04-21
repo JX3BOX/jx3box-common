734635ea60fadb686875d04117a66b530f69818d
iRuxu <rx6@qq.com>
2020-04-22 05:17:58 +0800

    sidebar mobile“

--- vue/index.js
+++ vue/index.js
@@ -2,13 +2,16 @@ import Top from './Top.vue'
 import Bottom from './Bottom.vue'
 import Header from './Header.vue'
 import Github_REPO from './Github_REPO.vue'
+import RightSideMsg from './RightSideMsg.vue'
+import LeftSideToggle from './LeftSideToggle.vue'
 
 const components = {
     Top,
     Bottom,
     Header,
     Github_REPO,
-    RightSideMsg
+    RightSideMsg,
+    LeftSideToggle
 }
 
 const install = function (Vue, Option) {
