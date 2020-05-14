1732918088f4060f56f276a6892b5b85913ae38c
iRuxu <rx6@qq.com>
2020-05-14 16:33:59 +0800

    fix data path & static path

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
-const {default_avatar,__dataPath} = require('./jx3box');
+const {default_avatar} = require('./jx3box');
 
 module.exports = {
     resolveImagePath : function (str){
@@ -30,20 +30,5 @@ module.exports = {
         return avatar + styleMap[size]
 
     },
-
-    dataPath :function (path,version="latest"){
-        return __dataPath + '@' + version + '/data/' + path
-    },
-
-    getQuery : function (key){
-        let val = ''
-		let params = new URLSearchParams(location.search);
-		let isRewrite = !params.get(key)
-		if(!isRewrite){
-			val = params.get(key)
-		}else{
-			val = location.pathname.slice(1).split('/').pop()
-		}
-		return val
-    }
+    
 }
