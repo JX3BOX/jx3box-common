2c920b4bf13a7ba356300b33e692cb155f3c1525
iRuxu <rx6@qq.com>
2020-05-20 08:23:03 +0800

    get query

--- js/utils.js
+++ js/utils.js
@@ -1,42 +1,56 @@
-const {default_avatar,__Links} = require('./jx3box');
+const { default_avatar, __Links } = require("./jx3box");
 
 module.exports = {
-    resolveImagePath : function (str){
-        return (str && str.length) ?  str.replace(/oss\.jx3box\.com/g,'console.cnyixun.com') : ''
+    resolveImagePath: function (str) {
+        return str && str.length
+            ? str.replace(/oss\.jx3box\.com/g, "console.cnyixun.com")
+            : "";
     },
 
-    checkImageLoad : function (jq){
+    checkImageLoad: function (jq) {
         jq.length &&
-        jq.one('error',function (){
-            var img_url = $(this).attr("src");
-            var fix_url = img_url.replace(
-                /console\.cnyixun\.com/g,
-                "oss.jx3box.com"
-            );
-            $(this).attr("src", fix_url);
-        })
+            jq.one("error", function () {
+                var img_url = $(this).attr("src");
+                var fix_url = img_url.replace(
+                    /console\.cnyixun\.com/g,
+                    "oss.jx3box.com"
+                );
+                $(this).attr("src", fix_url);
+            });
     },
 
-    showAvatar : function (url,size='s'){
-
+    showAvatar: function (url, size = "s") {
         const styleMap = {
-            s : "?x-oss-process=style/avatar_s",
-            m : "?x-oss-process=style/avatar_m",
-            l : "?x-oss-process=style/avatar_l"
-        }
+            s: "?x-oss-process=style/avatar_s",
+            m: "?x-oss-process=style/avatar_m",
+            l: "?x-oss-process=style/avatar_l",
+        };
 
-        let avatar = !url ? default_avatar : url.replace(/oss\.jx3box\.com/g,'console.cnyixun.com')
+        let avatar = !url
+            ? default_avatar
+            : url.replace(/oss\.jx3box\.com/g, "console.cnyixun.com");
 
-        return avatar + styleMap[size]
-
+        return avatar + styleMap[size];
     },
 
-    authorLink: function(uid) {
+    authorLink: function (uid) {
         return __Links.author + "?uid=" + uid;
     },
 
-    editLink : function (type,id){
-        return __Links.dashboard.publish + '#/' + type + '/' + id
-    }
-    
-}
+    editLink: function (type, id) {
+        return __Links.dashboard.publish + "#/" + type + "/" + id;
+    },
+
+    getQuery: function (key) {
+        let val = "";
+        let params = new URLSearchParams(location.search);
+        let isRewrite = !params.get(key);
+        if (!isRewrite) {
+            val = params.get(key);
+        } else {
+            let arr = location.pathname.slice(1).split("/");
+            val = arr[1];
+        }
+        return val;
+    },
+};
