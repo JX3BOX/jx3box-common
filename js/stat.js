ccb1e5a60d0b55cfc9d74a5c494e30f1ff73b6e9
iRuxu <rx6@qq.com>
2021-03-15 19:57:50 +0800

    fix stat

--- js/stat.js
+++ js/stat.js
@@ -1,13 +1,16 @@
 import axios from "axios";
-import { __sourceType } from "../data/jx3box.json";
+import { __sourceType,__next } from "../data/jx3box.json";
 const { cms_types } = __sourceType;
+const $next = axios.create({
+    baseURL: process.env.NODE_ENV === "production" ? __next : "/",
+});
 
 // 发送统计：同一个ip的同一篇文章的同一个动作10分钟内不重复统计
 function postStat(type, id, action = "views") {
     let api = cms_types.includes(type)
         ? "/api/summary-any/" + id
         : "/api/summary-any/" + type + "-" + id;
-    return axios.get(api, {
+    return $next.get(api, {
         params: {
             type: type,
             actions: action,
@@ -20,13 +23,13 @@ function getStat(type, id) {
     let api = cms_types.includes(type)
         ? "/api/summary-any/" + id + "/stat"
         : "/api/summary-any/" + type + "-" + id + "/stat";
-    return axios.get(api);
+    return $next.get(api);
 }
 
 
 // 获取统计
 function getStatRank(type, action = "views", limit = 10, sort = "7days") {
-    return axios.get("/api/summary/visit/rank", {
+    return $next.get("/api/summary/visit/rank", {
         params: {
             postType: type,
             postAction: action,
