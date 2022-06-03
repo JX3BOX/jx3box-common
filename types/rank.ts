57b2215f6697390abf71503f62c103a863951e29
Harper Gao <harper.gao@ringcentral.com>
2022-06-03 15:14:53 +0800

    Update rank-statistic types

--- types/rank.ts
+++ types/rank.ts
@@ -118,3 +118,45 @@ export interface Jx3boxRankEvent {
    */
   vote_note: string;
 }
+
+/**
+ * @number 服务器数量
+ * @string 服务器名称
+ * @type {Jx3boxServerAnalysisItem}
+ */
+type Jx3boxServerAnalysisItem = [number, string];
+interface Jx3boxBarServerAnalysis {
+  data: Array<Jx3boxServerAnalysisItem>;
+  /**
+   * 服务器列表
+   *
+   * @type {Array<string>}
+   * @memberof Jx3boxBarServerAnalysis
+   */
+  servers: Array<string>;
+}
+export interface Jx3boxAnalysisBar {
+  [name: string]: Jx3boxBarServerAnalysis;
+}
+
+interface Jx3boxAnalysisPieItem {
+  name: string;
+  value: number;
+}
+export interface Jx3boxAnalysisPie {
+  [name: string]: Array<Jx3boxAnalysisPieItem>;
+}
+
+export interface Jx3boxAnalysis {
+  bar_server_all: Jx3boxAnalysisBar;
+  bar_server_top10: Jx3boxAnalysisBar;
+  pie_dps_xf_ratio: Jx3boxAnalysisPie;
+  pie_hps_count: Jx3boxAnalysisPie;
+  pie_hps_xf_ratio: Jx3boxAnalysisPie;
+  pie_leader_type_ratio: Jx3boxAnalysisPie;
+  pie_neiwaigong_ratio: Jx3boxAnalysisPie;
+  pie_school_ratio: Jx3boxAnalysisPie;
+  pie_tank_count: Jx3boxAnalysisPie;
+  pie_tank_xf_ratio: Jx3boxAnalysisPie;
+  pie_xf_ratio: Jx3boxAnalysisPie;
+}
