177b9977dbf8a917b4faf840f34d77ebbf96a2e9
Kaviilee <jiaweilee95@126.com>
2022-06-12 00:16:55 +0800

    feat: wiki api update

--- js/wiki.js
+++ js/wiki.js
@@ -1,4 +1,10 @@
-import { $helper } from "./https";
+import { $helper, $node } from "./https";
+
+function getPet(petid, params) {
+    return $node().get(`/pet/${petid}`, {
+        params
+    });
+}
 
 const wiki = {
     // 获取最新攻略
@@ -55,7 +61,67 @@ const wiki = {
     // 删除我的百科攻略
     delete(post_id) {
         return $helper().put(`/api/my/wiki/post/${post_id}/remove`)
-    }
+    },
+    // 获取兼容攻略
+    handleMix(source_type, source_id, client, params) {
+        let post = '';
+        let source = '';
+        let isEmpty = true;
+        let compatible = false;
+        if (client === 'std') {
+            return this.get({ type: source_type, id: source_id }, {client, ...params}).then(res => {
+                post = res.data.data.post;
+                source = res.data.data.source;
+                post && (isEmpty = false);
+                console.log("获取重制攻略");
+                return { post, source, isEmpty, compatible };
+            })
+        } else {
+            return this.get({ type: source_type, id: source_id }, {client, ...params}).then(res => {
+                source = res.data.data.source;
+                post = res.data.data.post;
+                post && (isEmpty = false);
+                console.log("获取缘起攻略");
+                return !!res.data.data.post;
+            }).then(data => {
+                if (!data) {
+                    console.log("兼容：获取重制攻略");
+                    return this.get({ type: source_type, id: source_id }, {client: 'std', ...params}).then(res => {
+                        post = res.data.data.post;
+                        post && (isEmpty = false);
+                        compatible = true;
+                        return { post, source, isEmpty, compatible };
+                    })
+                }
+                return { post, source, isEmpty, compatible };
+            })
+        }
+    },
+    // 兼容怀旧服
+    mix({ type, id, client }, params) {
+        let source_type = '';
+        let source_id = '';
+        if (type === 'cj') {
+            source_type = 'achievement';
+        } else if (type === 'pet') {
+            source_type = 'item';
+        } else {
+            source_type = type || 'achievement';
+        }
+
+        if (id) {
+            source_id = id;
+            if (type === 'pet') {
+                getPet(id, { client }).then(res => {
+                    let pet = res.data;
+                    let source_id = pet.ItemTabType + "_" + pet.ItemTabIndex;
+                    return this.handleMix(source_type, source_id, client, params);
+                })
+            } else {
+                return this.handleMix(source_type, source_id, client, params);
+            }
+        }
+    },
 }
 
 const wikiComment = {
@@ -94,4 +160,4 @@ function getBread(channel) {
     return $helper().get(`/api/breadcrumb/${channel}`);
 }
 
-export { wiki, getBread }
+export { wiki, getBread, wikiComment };
