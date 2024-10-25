84f01de08b413888e31b21af6c2442aeaf94c045
Kaviilee <jiaweilee95@126.com>
2024-10-25 18:37:13 +0800

    feat: update stat

--- js/stat.js
+++ js/stat.js
@@ -36,4 +36,18 @@ function getStatRank(type, action = "views", limit = 10, sort = "7days") {
     });
 }
 
-export { getStat, postStat, getStatRank };
+// 历史记录
+/**
+ * 添加历史记录
+ * @param {*} data 
+ * @param {*} data.source_type 例如：macro,community
+ * @param {*} data.source_id 例如：文章id，帖子id
+ * @param {*} data.link 例如：文章链接，帖子链接
+ * @param {*} data.authorId 例如：作者id
+ * @returns 
+ */
+function postHistory(data) {
+    return $next.post("/api/next2/userdata/visit-history/item", data);
+}
+
+export { getStat, postStat, getStatRank, postHistory };
